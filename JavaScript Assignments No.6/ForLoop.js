89aaafb872110b24a7f07daaca32bc554177149c
AditiPawar7 <aditipawar6122004@gmail.com>
2025-10-01 23:25:51 +0530

    Add files via upload

---
+++ JavaScript Assignments No.6/ForLoop.js
@@ -0,0 +1,18 @@
+// Print numbers from 1 to 10
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// Print even numbers from 1 to 20
+for (let i = 2; i <= 20; i += 2) {
+    console.log(i);
+}
+
+// Star pattern
+for (let i = 1; i <= 4; i++) {
+    let pattern = "";
+    for (let j = 1; j <= i; j++) {
+        pattern += "* ";
+    }
+    console.log(pattern);
+}

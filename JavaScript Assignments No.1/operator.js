b196a90fabfd2cad700711b64345b239d0c2dd99
AditiPawar7 <aditipawar6122004@gmail.com>
2025-10-01 23:18:35 +0530

    Add files via upload

---
+++ JavaScript Assignments No.1/operator.js
@@ -0,0 +1,16 @@
+// Arithmetic operators
+let a = 20,b = 40;
+
+console.log("Addition",a+b);
+console.log("Substraction",a-b);
+console.log("Division",a/b);
+console.log("Multiplication",a*b);
+console.log("Modulus",a%b);
+
+
+// Relational operators
+console.log(a > b);   // true
+console.log(a < b);   // false
+console.log(a == 10); // true
+console.log(a === "10"); // false (strict equality checks type also)
+console.log(a != b);  // true

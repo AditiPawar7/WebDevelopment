97bbea1cb3273b68a410f9d06dee32e583a886f9
AditiPawar7 <aditipawar6122004@gmail.com>
2025-10-01 23:28:00 +0530

    Add files via upload

---
+++ JavaScript Assignments No.8/Dowhile.js
@@ -0,0 +1,12 @@
+// Print numbers from 1 to 5 using do...while loop
+let i = 1;
+do {
+    console.log(i);
+    i++;
+} while (i <= 5);
+
+// Program that prints "Welcome" at least once regardless of condition
+let condition = false;
+do {
+    console.log("Welcome");
+} while (condition);
